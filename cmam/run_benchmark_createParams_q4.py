e866211fc53f3030cbe7bc20fb65a4558a79a9dd
LiesVanDael <liesvandael@hotmail.com>
2026-02-18 13:23:13 +0100

    Add q4 run benchmark script for testing

---
+++ cmam/run_benchmark_createParams_q4.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from multiarea_model import MultiAreaModel
+from start_jobs import start_job # create parameter folders
+from chapter_experiments.q4_vanilla import network_params
+
+num_processes = int(sys.argv[1])
+local_num_threads = int(sys.argv[2])
+t_sim = float(sys.argv[3])
+rng_seed = int(sys.argv[4])
+data_path = sys.argv[5]
+label = sys.argv[6]
+record_spikes = sys.argv[7]
+
+sim_params = {'num_processes': num_processes,
+              'local_num_threads': local_num_threads,
+              'recording_dict': {'record_vm': False},
+              't_sim': t_sim,
+              'rng_seed': rng_seed 
+              }
+
+if not record_spikes:
+    sim_params['recording_dict']['areas_recorded'] = []
+
+os.makedirs(os.path.join(data_path, label), exist_ok=True)
+
+for net_params in network_params:
+    M = MultiAreaModel(net_params,
+                       simulation=True,
+                       sim_spec=sim_params,
+                       theory=False,
+                       analysis=False,
+                       data_path=data_path,
+                       data_folder_hash=label)
+
+print("M.label: ", M.label)
+print("M.simulation.label: ", M.simulation.label)
+
+start_job(M.simulation.label, data_path, data_folder_hash=label)
