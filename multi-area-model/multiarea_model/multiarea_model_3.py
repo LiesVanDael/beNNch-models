b6794c9ddedfb9f92a851d5b050e590699ff12c9
LiesVanDael <liesvandael@hotmail.com>
2026-06-30 16:08:09 +0200

    Set very small synapse numbers to zero

--- multi-area-model/multiarea_model/multiarea_model_3.py
+++ multi-area-model/multiarea_model/multiarea_model_3.py
@@ -155,8 +155,7 @@ class MultiAreaModel_3:
             ext = {area: {pop: ind[area][pop]['external'] for pop in
                           self.structure['V1']} for area in self.area_list}
             self.K = matrix_to_dict(
-                K_stable, self.area_list, self.structure, external=ext)
-            self.synapses = remove_TH_layer_4(zero_small_values(synapses_int(indegree_to_synapse_numbers(self.K, self.N)), tol=1e-12))
+                    K_stable, self.area_list, self.structure, external=ext)
 
         self.vectorize()
         if self.params['K_scaling'] != 1. or self.params['N_scaling'] != 1.:
