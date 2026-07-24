45ecaf586a400f7e1a0a617569ce9383881954c5
Melissa Lober <47082241+mlober@users.noreply.github.com>
2026-07-24 16:06:41 +0200

    Add poisson input as option

--- cmam/run_benchmark_createParams.py
+++ cmam/run_benchmark_createParams.py
@@ -13,6 +13,7 @@ data_path = sys.argv[5]
 label = sys.argv[6]
 record_spikes = sys.argv[7].lower() in ("true", "1", "yes")
 Q = int(sys.argv[8])
+poisson_input = sys.argv[9].lower() in ("true", "1", "yes")
 
 sim_params = {'num_processes': num_processes,
               'local_num_threads': local_num_threads,
@@ -27,6 +28,7 @@ if not record_spikes:
 os.makedirs(os.path.join(data_path, label), exist_ok=True)
 
 network_params = params(Q)
+network_params['input_params']['poisson_input'] = poisson_input
 
 for net_params in network_params:
     M = MultiAreaModel(net_params,
