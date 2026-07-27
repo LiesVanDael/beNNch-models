aa761b527700196b1060ddf22c30a0846c0f2068
Melissa Lober <47082241+mlober@users.noreply.github.com>
2026-07-27 11:10:03 +0200

    Update run_benchmark_createParams.py

--- cmam/run_benchmark_createParams.py
+++ cmam/run_benchmark_createParams.py
@@ -13,7 +13,6 @@ data_path = sys.argv[5]
 label = sys.argv[6]
 record_spikes = sys.argv[7].lower() in ("true", "1", "yes")
 Q = int(sys.argv[8])
-poisson_input = sys.argv[9].lower() in ("true", "1", "yes")
 
 sim_params = {'num_processes': num_processes,
               'local_num_threads': local_num_threads,
@@ -30,7 +29,6 @@ os.makedirs(os.path.join(data_path, label), exist_ok=True)
 network_params = params(Q)
 
 for net_params in network_params:
-    network_params['input_params']['poisson_input'] = poisson_input
     M = MultiAreaModel(net_params,
                        simulation=True,
                        sim_spec=sim_params,
